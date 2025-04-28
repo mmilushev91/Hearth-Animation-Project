5c1d1cdf771cedc0c097af67b6c75d181f90c128
mmilushev91 <milushev_1990@abv.bg>
2025-04-28 22:28:01 +0300

    Add files via upload

---
+++ heath-trail-animation/script.js
@@ -0,0 +1,18 @@
+"using strict";
+
+const bodyElement = document.querySelector("body");
+
+bodyElement.addEventListener("mousemove", function (event) {
+  const xPos = event.offsetX;
+  const yPos = event.offsetY;
+  const spanEl = document.createElement("span");
+  spanEl.style.left = xPos + "px";
+  spanEl.style.top = yPos + "px";
+  const size = Math.random() * 100;
+  spanEl.style.width = size + "px";
+  spanEl.style.height = size + "px";
+  bodyElement.appendChild(spanEl);
+  setTimeout(() => {
+    spanEl.remove();
+  }, 3000);
+});
