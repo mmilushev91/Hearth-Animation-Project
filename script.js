69270c0268ffc74cef3deb56d2b04bc1a680c2e5
mmilushev91 <milushev_1990@abv.bg>
2025-04-29 14:52:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-"using strict";
+const bodyEl = document.querySelector("body");
 
-const bodyElement = document.querySelector("body");
+bodyEl.addEventListener("mousemove", function(event) {
 
-bodyElement.addEventListener("mousemove", function (event) {
   const xPos = event.offsetX;
   const yPos = event.offsetY;
+  
   const spanEl = document.createElement("span");
+  
   spanEl.style.left = xPos + "px";
   spanEl.style.top = yPos + "px";
+  
   const size = Math.random() * 100;
+  
   spanEl.style.width = size + "px";
   spanEl.style.height = size + "px";
-  bodyElement.appendChild(spanEl);
+  
+  bodyEl.appendChild(spanEl);
   setTimeout(() => {
     spanEl.remove();
   }, 3000);
-});
+})
